817fee78479f29b82b2cc93d402e1da8fba27b43
Isaac Luis Silva Santos <shurikenops@gmail.com>
2021-12-07 08:11:13 -0300

    Praticas

--- Pratica37/src/Pratica37.java
+++ Pratica37/src/Pratica37.java
@@ -16,16 +16,16 @@ public class Pratica37 {
 		for (int i=0; i < vet.length; i++ ) {
 			for (int j=0; j < vet[i].length; j++) {
 				if (i==0 && j<2) {
-					vet[i][j] = String.format("%09d",vet2[i][j]);
+					vet[i][j] = imprimirBonito(vet2[i][j]);
 				} else if (i>0&&j==0) {
 					vet2[i][j] = vet2[i-1][vet2[i].length-1] + vet2[i-1][vet2[i].length-2];
-					vet[i][j] = String.format("%09d",vet2[i][j]);
+					vet[i][j] = imprimirBonito(vet2[i][j]);
 				} else if (j==1){
 					vet2[i][j] = vet2[i][j-1] + vet2[i-1][vet2[i].length-1];
-					vet[i][j] = String.format("%09d",vet2[i][j]);
+					vet[i][j] = imprimirBonito(vet2[i][j]);
 				} else {
 					vet2[i][j] = vet2[i][j-1] + vet2[i][j-2];
-					vet[i][j] = String.format("%09d",vet2[i][j]);
+					vet[i][j] = imprimirBonito(vet2[i][j]);
 				}
 			}
 		}		
@@ -39,6 +39,10 @@ public class Pratica37 {
 			}
 			System.out.println("|\n");
 		}
+	}
+	
+	public static String imprimirBonito(int v) {
+		return String.format("%09d",v);
 	}
 
 }
